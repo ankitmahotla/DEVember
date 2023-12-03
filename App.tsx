97397312cd17498428c61d566f16b383fe395194
ankitmahotla <ankitmahotla45@gmail.com>
2023-12-03 12:09:42 +0530

    Getting Started with DEVember

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+const days = [1, 6];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      {days.map((day) => (
+        <View style={styles.box}>
+          <Text style={styles.text}>{day}</Text>
+        </View>
+      ))}
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +19,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  box: {
+    backgroundColor: "#F9EDE3",
+    width: 300,
+    height: 300,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: "#9B4521",
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "#9B4521",
+    fontSize: 70,
   },
 });
