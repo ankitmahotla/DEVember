3bcdecf3c5b2a1b491ca855bb75af2cf8e5c03a3
ankitmahotla <ankitmahotla45@gmail.com>
2023-12-04 12:22:47 +0530

    Stack Navigator

--- src/app/(days)/day1/index.tsx
+++ src/app/(days)/day1/index.tsx
@@ -1,8 +1,10 @@
+import { Stack } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function index() {
   return (
     <View>
+      <Stack.Screen options={{ title: "Day 1" }} />
       <Text>Day 1</Text>
     </View>
   );
