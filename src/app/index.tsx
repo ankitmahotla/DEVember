0c217364b17f023603e8f7819c734080f7de8503
ankitmahotla <ankitmahotla45@gmail.com>
2023-12-04 12:15:33 +0530

    Setup Expo Router and the Main Screens

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ import {
   Text,
   View,
 } from "react-native";
-import DayListItem from "./src/components/core/DayListItem";
+import DayListItem from "../../src/components/core/DayListItem";
 import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
@@ -19,7 +19,7 @@ SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
-export default function App() {
+export default function HomeScreen() {
   const [fontsLoaded, fontError] = useFonts({
     Inter: Inter_900Black,
     Amatic: AmaticSC_400Regular,
