b53b3544590e1ba175395fa723d9f9220e5e875e
ankitmahotla <ankitmahotla45@gmail.com>
2023-12-06 12:59:35 +0530

    minor changes

--- src/app/(days)/day2/onboarding.tsx
+++ src/app/(days)/day2/onboarding.tsx
@@ -184,7 +184,7 @@ const styles = StyleSheet.create({
   stepIndicatorContainer: {
     flexDirection: "row",
     gap: 8,
-    marginTop: 25,
+    marginTop: 60,
     marginHorizontal: 15,
   },
   stepIndicator: {
