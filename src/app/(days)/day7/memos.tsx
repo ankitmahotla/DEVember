7989d7f3198b95bc25af753d1773bb5e6275db0f
ankitmahotla <ankitmahotla45@gmail.com>
2023-12-21 00:15:26 +0530

    Day 7: Voice Memos Completed

--- src/app/(days)/day7/memos.tsx
+++ src/app/(days)/day7/memos.tsx
@@ -1,40 +1,62 @@
 import { useState } from "react";
-import { View, StyleSheet, Button, Text, Pressable } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  FlatList,
+  Pressable,
+} from "react-native";
 import { Audio } from "expo-av";
 import { Recording } from "expo-av/build/Audio";
-import { FlatList } from "react-native-gesture-handler";
 import Animated, {
+  interpolate,
   useAnimatedStyle,
+  useSharedValue,
+  withSpring,
   withTiming,
 } from "react-native-reanimated";
-import MemoListItem from "@/components/day7/MemoListItem";
+import MemoListItem, { Memo } from "@/components/day7/MemoListItem";
 
 export default function MemosScreen() {
   const [recording, setRecording] = useState<Recording>();
-  const [memos, setMemos] = useState<string[]>([]);
+  const [memos, setMemos] = useState<Memo[]>([]);
+
+  const [audioMetering, setAudioMetering] = useState<number[]>([]);
+  const metering = useSharedValue(-100);
 
   async function startRecording() {
     try {
-      console.log("Requesting permissions..");
+      setAudioMetering([]);
+
       await Audio.requestPermissionsAsync();
       await Audio.setAudioModeAsync({
         allowsRecordingIOS: true,
         playsInSilentModeIOS: true,
       });
 
-      console.log("Starting recording..");
       const { recording } = await Audio.Recording.createAsync(
-        Audio.RecordingOptionsPresets.HIGH_QUALITY
+        Audio.RecordingOptionsPresets.HIGH_QUALITY,
+        undefined,
+        100
       );
       setRecording(recording);
-      console.log("Recording started");
+
+      recording.setOnRecordingStatusUpdate((status) => {
+        if (status.metering) {
+          metering.value = status.metering;
+          setAudioMetering((curVal) => [...curVal, status.metering || -100]);
+        }
+      });
     } catch (err) {
       console.error("Failed to start recording", err);
     }
   }
 
   async function stopRecording() {
-    if (!recording) return;
+    if (!recording) {
+      return;
+    }
 
     console.log("Stopping recording..");
     setRecording(undefined);
@@ -44,30 +66,55 @@ export default function MemosScreen() {
     });
     const uri = recording.getURI();
     console.log("Recording stopped and stored at", uri);
-
+    metering.value = -100;
     if (uri) {
-      setMemos((existingMemos) => [uri, ...existingMemos]);
+      setMemos((existingMemos) => [
+        { uri, metering: audioMetering },
+        ...existingMemos,
+      ]);
     }
   }
 
-  const animatedCircle = useAnimatedStyle(() => ({
+  const animatedRedCircle = useAnimatedStyle(() => ({
     width: withTiming(recording ? "60%" : "100%"),
     borderRadius: withTiming(recording ? 5 : 35),
   }));
+
+  const animatedRecordWave = useAnimatedStyle(() => {
+    const size = withTiming(
+      interpolate(metering.value, [-160, -60, 0], [0, 0, -30]),
+      { duration: 100 }
+    );
+    return {
+      top: size,
+      bottom: size,
+      left: size,
+      right: size,
+      backgroundColor: `rgba(255, 45, 0, ${interpolate(
+        metering.value,
+        [-160, -60, -10],
+        [0.7, 0.3, 0.7]
+      )})`,
+    };
+  });
 
   return (
     <View style={styles.container}>
       <FlatList
         data={memos}
-        renderItem={({ item }) => <MemoListItem uri={item} />}
+        renderItem={({ item }) => <MemoListItem memo={item} />}
       />
+
       <View style={styles.footer}>
-        <Pressable
-          style={styles.recordButton}
-          onPress={recording ? stopRecording : startRecording}
-        >
-          <Animated.View style={[styles.redCircle, animatedCircle]} />
-        </Pressable>
+        <View>
+          <Animated.View style={[styles.recordWave, animatedRecordWave]} />
+          <Pressable
+            style={styles.recordButton}
+            onPress={recording ? stopRecording : startRecording}
+          >
+            <Animated.View style={[styles.redCircle, animatedRedCircle]} />
+          </Pressable>
+        </View>
       </View>
     </View>
   );
@@ -78,7 +125,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     backgroundColor: "#ecf0f1",
-    padding: 10,
   },
   footer: {
     backgroundColor: "white",
@@ -87,19 +133,29 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   recordButton: {
-    width: 60,
-    height: 60,
-    borderRadius: 60,
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+
     borderWidth: 3,
     borderColor: "gray",
     padding: 3,
+
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor: "white",
   },
+  recordWave: {
+    position: "absolute",
+    top: -20,
+    bottom: -20,
+    left: -20,
+    right: -20,
+    borderRadius: 1000,
+  },
+
   redCircle: {
     backgroundColor: "orangered",
-    width: 60,
     aspectRatio: 1,
-    borderRadius: 30,
   },
 });
